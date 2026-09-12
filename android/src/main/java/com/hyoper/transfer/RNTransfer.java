af6be2fecf22a544ba65df8d7565f7f58c903598
Tolgahan Çelik <tol_celik@hotmail.com>
2026-09-12 20:19:52 +0300

    RNTransfer.java and RNTransfer.mm codegen implement all members.

--- android/src/main/java/com/hyoper/transfer/RNTransfer.java
+++ android/src/main/java/com/hyoper/transfer/RNTransfer.java
@@ -1,8 +1,12 @@
 package com.hyoper.transfer;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.module.annotations.ReactModule;
 
 import com.hyoper.transfer.helpers.RNTransferUtils;
@@ -25,5 +29,77 @@ public class RNTransfer extends NativeRNTransferSpec {
     @Override
     public String getName() {
         return NAME;
+    }
+
+    @Override
+    public WritableArray getDownloads() {
+        return null;
+    }
+
+    @Override
+    public boolean clearDownloads() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public WritableMap getDownload(String id) {
+        return null;
+    }
+
+    @Override
+    public WritableMap createDownload(ReadableMap options) {
+        return null;
+    }
+
+    @Override
+    public WritableMap removeDownload(String id) {
+        return null;
+    }
+
+    @Override
+    public void startDownload(String id) {
+
+    }
+
+    @Override
+    public void stopDownload(String id) {
+
+    }
+
+    @Override
+    public WritableArray getUploads() {
+        return null;
+    }
+
+    @Override
+    public boolean clearUploads() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public WritableMap getUpload(String id) {
+        return null;
+    }
+
+    @Override
+    public WritableMap createUpload(ReadableMap options) {
+        return null;
+    }
+
+    @Override
+    public WritableMap removeUpload(String id) {
+        return null;
+    }
+
+    @Override
+    public void startUpload(String id) {
+
+    }
+
+    @Override
+    public void stopUpload(String id) {
+
     }
 }
--- ios/RNTransfer.mm
+++ ios/RNTransfer.mm
@@ -22,6 +22,72 @@
     [RNTransferUtils reset];
 }
 
+- (NSArray *)getDownloads
+{
+    return nil;
+}
+
+- (NSNumber *)clearDownloads
+{
+    return @NO;
+}
+
+- (NSDictionary *)getDownload:(NSString *)identifier
+{
+    return nil;
+}
+
+- (NSDictionary *)createDownload:(JS::NativeRNTransfer::DownloadOptions &)options
+{
+    return nil;
+}
+
+- (NSDictionary *)removeDownload:(NSString *)identifier
+{
+    return nil;
+}
+
+- (void)startDownload:(NSString *)identifier
+{
+}
+
+- (void)stopDownload:(NSString *)identifier
+{
+}
+
+- (NSArray *)getUploads
+{
+    return nil;
+}
+
+- (NSNumber *)clearUploads
+{
+    return @NO;
+}
+
+- (NSDictionary *)getUpload:(NSString *)identifier
+{
+    return nil;
+}
+
+- (NSDictionary *)createUpload:(JS::NativeRNTransfer::UploadOptions &)options
+{
+    return nil;
+}
+
+- (NSDictionary *)removeUpload:(NSString *)identifier
+{
+    return nil;
+}
+
+- (void)startUpload:(NSString *)identifier
+{
+}
+
+- (void)stopUpload:(NSString *)identifier
+{
+}
+
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const facebook::react::ObjCTurboModule::InitParams &)params {
     return std::make_shared<facebook::react::NativeRNTransferSpecJSI>(params);
 }
